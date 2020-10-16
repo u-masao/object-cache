f041b40d81d0247d5ed1c70b74d420ecb8b83995
u-masao <4973920+u-masao@users.noreply.github.com>
2020-10-16 16:04:24 +0900

    add test code

--- object-cache/tests/test_object_cache.py
+++ object-cache/tests/test_object_cache.py
@@ -1,5 +1,29 @@
-from object_cache import __version__
+from object_cache import object_cache
+import object_cache as oc
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@object_cache
+def add(a, b):
+    return a + b
+
+
+def test_cache_hit():
+    assert add(1, 2) == 3
+    assert add(2, 3) == 5
+    assert add(1, 2) == 3
+
+
+def test_enable_print():
+    oc.enable_print = True
+    assert add(1, 2) == 3
+    assert add(2, 3) == 5
+    assert add(1, 2) == 3
+
+
+def test_disable():
+    oc.disable_cache = True
+    assert add(1, 2) == 3
+    assert add(2, 3) == 5
+    assert add(1, 2) == 3
+
+
